61f5ead91918d6b418fd926a76c4dd51b8055a42
Vlad Moyseenko <vlad.dancer@gmail.com>
2016-07-07 19:47:23 +0300

    Remove old code for test runner

--- src/Tests/Integration/runtests.php
+++ src/Tests/Integration/runtests.php
@@ -4,25 +4,5 @@
  * Test runner.
  */
 
-use Drupal\Core\DrupalKernel;
-use Symfony\Component\HttpFoundation\Request;
-
-$drupal_dir = getenv('DRUPAL_DIR');
-
-//static $kernel;
-
-/*if (!isset($kernel) || !($kernel instanceof DrupalKernel)) {
-  require_once $drupal_dir . '/core/includes/database.inc';
-  require_once $drupal_dir . '/core/includes/schema.inc';
-
-  $autoloader = require_once $drupal_dir . '/autoload.php';
-  $request    = Request::createFromGlobals();
-
-  $kernel = DrupalKernel::createFromRequest($request, $autoloader, 'prod');
-  $kernel->boot();
-  $kernel->prepareLegacyRequest($request);
-}*/
-
-//require_once './CanSendEmailToMailtrapSmtp.php';
 $mailer = new \Drupal\netsmtp\Tests\Integration\CanSendEmailToMailtrapSmtp();
 $mailer->testSendEmail();
